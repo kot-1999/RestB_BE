f53e3c44586b42d61a3b2d63dcbf38c08176fbd5
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-18 16:12:14 +0000

    routes for b2c booking and restaurant were created

--- src/routes/b2c/BookingRouter.ts
+++ src/routes/b2c/BookingRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { BookingController } from '../../controllers/b2b/v1/booking/BookingController';
+import { BookingController } from '../../controllers/b2c/v1/booking/BookingController';
 import authorizationMiddleware from '../../middlewares/authorizationMiddleware'
 import validationMiddleware from '../../middlewares/validationMiddleware'
 import { PassportStrategy } from '../../utils/enums'
@@ -11,23 +11,6 @@ const bookingController = new BookingController()
 
 export default function bookingRouter() {
     // List endpoints
-    router.get(
-        /*
-            #swagger.tags = ['b2c-v1-Booking']
-            #swagger.description = 'Get Booking details',
-            #swagger.parameters['body'] = {
-                in: 'body',
-                schema: { $ref: '#/definitions/b2cV1GetBookingReqBody' }
-            }
-            #swagger.responses[200] = {
-                schema: { "$ref": "#/definitions/b2cV1GetBookingRes" },
-            }
-        */
-        '/:bookingID',
-        validationMiddleware(BookingController.schemas.request.getBooking),
-        authorizationMiddleware([PassportStrategy.jwtB2c, PassportStrategy.google]),
-        bookingController.getBooking
-    )
 
     router.get(
         /*
