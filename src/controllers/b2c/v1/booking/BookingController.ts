341de7b6b2d17d01ff08f80a6bcb88c75c19a6bd
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-18 16:11:27 +0000

    b2c Booking and Restaurant controllers were added

---
+++ src/controllers/b2c/v1/booking/BookingController.ts
@@ -0,0 +1,107 @@
+import { BookingStatus } from '@prisma/client';
+import { Response, NextFunction, AuthUserRequest } from 'express'
+import Joi from 'joi'
+
+import { AbstractController } from '../../../../types/AbstractController'
+import { JoiCommon } from '../../../../types/JoiCommon'
+
+export class BookingController extends AbstractController {
+    public static readonly schemas = {
+        request: {
+            getBookingList: JoiCommon.object.request.keys({
+                query: Joi.object({
+                    dateFrom: Joi.date().iso(),
+
+                    dateTo: Joi.date()
+                        .iso()
+                        .min(Joi.ref('dateFrom')),
+
+                    statuses: Joi.array().items(Joi.string().valid(...Object.values(BookingStatus))),
+
+                    page: Joi.number()
+                        .integer()
+                        .min(1)
+                        .default(1),
+
+                    limit: Joi.number()
+                        .integer()
+                        .min(1)
+                        .max(100)
+                        .default(10)
+                }).required()
+            }),
+            postBooking: JoiCommon.object.request.keys({
+                body: Joi.object({
+                    restaurantID: JoiCommon.string.id,
+                    guestsNumber: Joi.number().min(1)
+                        .integer()
+                        .required(),
+                    bookingTime: Joi.date().iso()
+                        .required(),
+                    discussion: JoiCommon.object.discussionItem.keys({
+                        createdAt: Joi.forbidden()
+                    }).optional()
+                }).required()
+            })
+        },
+        response: {
+            getBookingList: Joi.array().items(JoiCommon.object.booking.keys({
+                restaurant: Joi.object({
+                    id: JoiCommon.string.id,
+                    name: JoiCommon.string.name.required(),
+                    description: Joi.string().min(20)
+                        .optional(),
+                    bannerURL: Joi.string().uri()
+                        .required(),
+                    brand: JoiCommon.object.brand.required()
+                }).required(),
+                createdAt: Joi.date().iso()
+                    .required(),
+                updatedAt: Joi.date().iso()
+                    .allow(null)
+                    .required()
+            }).required())
+                .required(),
+            postBooking: Joi.object({
+                booking: Joi.object({
+                    id: JoiCommon.string.id
+                }).required(),
+                message: Joi.string().required()
+            })
+        }
+    }
+
+    constructor() {
+        super()
+    }
+
+    private GetBookingListReqType: Joi.extractType<typeof BookingController.schemas.request.getBookingList>
+    private GetBookingListResType: Joi.extractType<typeof BookingController.schemas.response.getBookingList>
+    getBookingList(
+        req: AuthUserRequest & typeof this.GetBookingListReqType,
+        res: Response<typeof this.GetBookingListResType>,
+        next: NextFunction
+    ) {
+        try {
+
+            return res.status(200).json({ message: 'Response from a backend template' })
+        } catch (err) {
+            return next(err)
+        }
+    }
+
+    private PostBookingReqType: Joi.extractType<typeof BookingController.schemas.request.postBooking>
+    private PostBookingResType: Joi.extractType<typeof BookingController.schemas.response.postBooking>
+    postBooking(
+        req: AuthUserRequest & typeof this.PostBookingReqType,
+        res: Response<typeof this.PostBookingResType>,
+        next: NextFunction
+    ) {
+        try {
+
+            return res.status(200).json({ message: 'Response from a backend template' })
+        } catch (err) {
+            return next(err)
+        }
+    }
+}
