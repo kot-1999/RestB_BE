7268ed670b771b3531b20c214bf9c2ec5b0ef42e
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-21 20:05:35 +0000

    queries classes were moved

--- src/database/queries/AdminQueries.ts
+++ src/database/queries/AdminQueries.ts
@@ -1,6 +1,6 @@
 import { Prisma, Admin, PrismaClient } from '@prisma/client'
 
-import { BaseQueries } from '../../../../utils/BaseQueries';
+import { BaseQueries } from './BaseQueries';
 
 export default class AdminQueries extends BaseQueries<
     Admin,
