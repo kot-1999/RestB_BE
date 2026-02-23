feb6e35953068355cc56a0413d89ba907d839670
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-23 01:39:19 +0000

    eslint was fixed

--- src/services/AwsS3.ts
+++ src/services/AwsS3.ts
@@ -3,7 +3,7 @@ import { randomUUID } from 'crypto'
 import {
     S3Client,
     CreateBucketCommand,
-    HeadBucketCommand, PutObjectCommand, GetObjectCommand, PutBucketCorsCommand
+    HeadBucketCommand, PutObjectCommand, PutBucketCorsCommand
 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import config from 'config';
