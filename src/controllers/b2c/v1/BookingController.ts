57861bf168fb09ccfa9996963d868c68dfb4a6ad
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-03-05 22:43:12 +0000

    endpoints to create booking and get bookings list for b2c user were implemented

--- src/controllers/b2c/v1/BookingController.ts
+++ src/controllers/b2c/v1/BookingController.ts
@@ -1,24 +1,34 @@
-// @ts-nocheck
-
-import { BookingStatus } from '@prisma/client';
+import { Admin, Booking, BookingStatus, Restaurant, User } from '@prisma/client';
+import dayjs from 'dayjs';
 import { Response, NextFunction, AuthUserRequest } from 'express'
 import Joi from 'joi'
 
+import prisma from '../../../services/Prisma';
 import { AbstractController } from '../../../types/AbstractController'
 import { JoiCommon } from '../../../types/JoiCommon'
+import { AuthorType } from '../../../utils/enums';
+import { IError } from '../../../utils/IError';
+
+const allowedStatuses: BookingStatus[]
+    = [BookingStatus.Approved, BookingStatus.Pending, BookingStatus.NoShow, BookingStatus.Completed, BookingStatus.Cancelled]
 
 export class BookingController extends AbstractController {
     public static readonly schemas = {
         request: {
             getBookingList: JoiCommon.object.request.keys({
                 query: Joi.object({
-                    dateFrom: Joi.date().iso(),
+                    dateFrom: Joi.date().iso()
+                        .default(dayjs().toISOString()),
 
                     dateTo: Joi.date()
                         .iso()
-                        .min(Joi.ref('dateFrom')),
+                        .min(Joi.ref('dateFrom'))
+                        .default(dayjs().add(7, 'days')
+                            .toISOString()),
 
-                    statuses: Joi.array().items(Joi.string().valid(...Object.values(BookingStatus))),
+                    statuses: Joi.array()
+                        .items(Joi.string().valid(...allowedStatuses as BookingStatus[]))
+                        .default(() => allowedStatuses),
 
                     page: Joi.number()
                         .integer()
@@ -39,16 +49,22 @@ export class BookingController extends AbstractController {
                         .integer()
                         .required(),
                     bookingTime: Joi.date().iso()
+                        .min('now')
                         .required(),
-                    discussion: JoiCommon.object.discussionItem.keys({
-                        createdAt: Joi.forbidden()
-                    }).optional()
+                    message: Joi.string().trim()
+                        .min(5)
+                        .optional()
                 }).required()
             })
         },
         response: {
             getBookingList: Joi.object({
                 bookings: Joi.array().items(JoiCommon.object.booking.keys({
+                    discussion: Joi.array().items(JoiCommon.object.discussionItem.keys({
+                        firstName: JoiCommon.string.name.required(),
+                        lastName: JoiCommon.string.name.required(),
+                        avatarURL: Joi.string().uri()
+                    })),
                     restaurant: Joi.object({
                         id: JoiCommon.string.id,
                         name: JoiCommon.string.companyName.required(),
@@ -80,14 +96,133 @@ export class BookingController extends AbstractController {
 
     private GetBookingListReqType: Joi.extractType<typeof BookingController.schemas.request.getBookingList>
     private GetBookingListResType: Joi.extractType<typeof BookingController.schemas.response.getBookingList>
+
     public async getBookingList(
         req: AuthUserRequest & typeof this.GetBookingListReqType,
         res: Response<typeof this.GetBookingListResType>,
         next: NextFunction
     ) {
         try {
+            const { query, user } = req
 
-            return res.status(200).json({ message: 'Response from a backend template' })
+            const timeFrom = dayjs(query.dateFrom).startOf('day')
+                .toDate()
+
+            const timeTo = dayjs(query.dateTo).endOf('day')
+                .toDate()
+
+            const skip = (query.page - 1) * query.limit;
+
+            const bookings = await prisma.booking.findMany({
+                skip,
+                take: query.limit,
+                where: {
+                    AND: [
+                        {
+                            status: {
+                                in: query.statuses
+                            }
+                        },
+                        {
+                            userID: user.id
+                        },
+                        {
+                            bookingTime: {
+                                gte: timeFrom,
+                                lt: timeTo
+                            }
+                        }
+                    ]
+                },
+                orderBy: {
+                    bookingTime: 'asc' // optional
+                },
+                select: {
+                    id: true,
+                    guestsNumber: true,
+                    bookingTime: true,
+                    status: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    discussion: true,
+                    restaurant: {
+                        select: {
+                            id: true,
+                            name: true,
+                            description: true,
+                            bannerURL: true,
+                            brand: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                    logoURL: true
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+
+            const adminIDs: string[] = []
+
+            for (let i = 0; i < bookings.length; i++) {
+                if (bookings[i].discussion) {
+                    bookings[i].discussion.forEach((discussion: { authorID: string, authorType: string}) => {
+                        if (discussion.authorType === AuthorType.Admin) {
+                            adminIDs.push(discussion.authorID)
+                        }
+                    })
+                }
+            }
+            const admins: Admin[] = await prisma.admin.findMany({
+                where: {
+                    id: {
+                        in: adminIDs
+                    }
+                },
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    avatarURL: true
+                }
+            })
+            return res.status(200).json({
+                bookings: bookings.map((booking: Booking & { restaurant: Restaurant}) => ({
+                    id: booking.id,
+                    guestsNumber: booking.guestsNumber,
+                    bookingTime: booking.bookingTime,
+                    status: booking.status,
+                    createdAt: booking.createdAt,
+                    updatedAt: booking.updatedAt,
+                    discussion: booking.discussion ? (booking.discussion as any[]).map((discussion) => {
+                        let author: Partial<Admin> | Partial<User> = user
+
+                        if (discussion.authorType === AuthorType.Admin) {
+                            const resultAdmin = admins.find((admin) => admin.id === discussion.authorID)
+                            if (resultAdmin) {
+                                author = resultAdmin
+                            } else {
+                                author = {
+                                    firstName: 'Unknown',
+                                    lastName: 'Administrator'
+                                }
+                            }
+                        }
+
+                        return {
+                            authorID: discussion.authorID,
+                            authorType: discussion.authorType,
+                            message: discussion.message,
+                            createAt: discussion.createAt,
+                            firstName: author.firstName,
+                            lastName: author.lastName,
+                            avatarUrl: author?.avatarURL
+                        }
+                    }) : [],
+                    restaurant: booking.restaurant
+                }))
+            })
         } catch (err) {
             return next(err)
         }
@@ -101,12 +236,74 @@ export class BookingController extends AbstractController {
         next: NextFunction
     ) {
         try {
+            const { user, body } = req
+
+            // Check if restaurant exists
+
+            const [restaurant, bookingSum] = await Promise.all([
+                prisma.restaurant.findByID(body.restaurantID, {
+                    id: true,
+                    timeFrom: true,
+                    timeTo: true,
+                    autoApprovedBookingsNum: true
+                }) as Promise<Restaurant>,
+                prisma.booking.groupBy({
+                    by: ['restaurantID'],
+                    _sum: { guestsNumber: true },
+                    where: {
+                        restaurantID: body.restaurantID,
+                        status: {
+                            in: [BookingStatus.Approved, BookingStatus.Completed]
+                        },
+                        bookingTime: {
+                            gte: dayjs(body.bookingTime).startOf('day')
+                                .toDate(),
+                            lt: dayjs(body.bookingTime).endOf('day')
+                                .toDate()
+                        }
+                    }
+                })
+            ])
+
+            if (!restaurant) {
+                throw new IError(404, 'Restaurant not found')
+            }
+
+            // Check if restaurant accepts booking at given time
+            const bookingHHmm = dayjs(body.bookingTime).format('HH:mm')
+            const { timeFrom, timeTo } = restaurant
+            const inRange = timeFrom <= timeTo
+                ? bookingHHmm >= timeFrom && bookingHHmm <= timeTo
+                : bookingHHmm >= timeFrom || bookingHHmm <= timeTo
+
+            if (!inRange) {
+                throw new IError(400, `Booking must be between ${timeFrom} and ${timeTo}`)
+            }
+
+            const peopleTotallyBooked: number = bookingSum[0]?._sum.guestsNumber ?? 0
+            const isAutoApproved = (restaurant.autoApprovedBookingsNum - (peopleTotallyBooked + body.guestsNumber)) >= 0
+
+            const booking = await prisma.booking.createOne({
+                guestsNumber: body.guestsNumber,
+                bookingTime: body.bookingTime,
+                status: isAutoApproved ?  BookingStatus.Approved : BookingStatus.Pending,
+                discussion: body.message ? [{
+                    authorID: user.id,
+                    authorType: AuthorType.User,
+                    message: body.message,
+
+
+                    createdAt: dayjs().toISOString()
+                }] : [],
+                userID: user.id,
+                restaurantID: body.restaurantID
+            })
 
             return res.status(200).json({
                 booking: {
-                    id: '123'
+                    id: booking.id
                 },
-                message: 'Response from a backend template' 
+                message: 'Response from a backend template'
             })
         } catch (err) {
             return next(err)
---
+++ tests/cases/controllers/b2c/v1/bookingController.test.ts
@@ -0,0 +1,201 @@
+import { faker } from '@faker-js/faker';
+import { BookingStatus, Restaurant, User } from '@prisma/client'
+import { expect } from 'chai'
+import dayjs from 'dayjs';
+import supertest from 'supertest'
+
+import app from '../../../../../src/app'
+import { BookingController } from '../../../../../src/controllers/b2c/v1/BookingController'
+import { JwtService } from '../../../../../src/services/Jwt'
+import prisma from '../../../../../src/services/Prisma'
+import { JwtAudience } from '../../../../../src/utils/enums'
+
+const endpoint = (val: string = '') => '/api/b2c/v1/booking/' + val
+
+describe('GET ' + endpoint(), () => {
+    let user: User 
+    
+    before(async () => {
+        user = await prisma.user.findOne()
+    })
+    
+    it('List of bookings (200)', async () => {
+        const res = await supertest(app)
+            .get(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+        expect(res.statusCode).to.equal(200)
+        expect(res.type).to.eq('application/json')
+
+        const validationResult = BookingController.schemas.response.getBookingList.validate(res.body)
+        expect(validationResult.error).to.eq(undefined)
+    })
+
+    it('List of bookings with query params (200)', async () => {
+        const res = await supertest(app)
+            .get(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .query({
+                page: 2,
+                limit: 3,
+                statuses: [BookingStatus.Approved, BookingStatus.Pending, BookingStatus.Completed],
+                dateFrom: dayjs().subtract(20, 'days')
+                    .toISOString(),
+                dateTo: dayjs().add(20, 'days')
+                    .toISOString()
+            })
+
+        expect(res.statusCode).to.equal(200)
+        expect(res.type).to.eq('application/json')
+
+        const validationResult = BookingController.schemas.response.getBookingList.validate(res.body)
+        expect(validationResult.error).to.eq(undefined)
+    })
+
+    it('Date from must be lover than dateTo (200)', async () => {
+        const res = await supertest(app)
+            .get(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .query({
+                page: 2,
+                limit: 3,
+                statuses: [BookingStatus.Approved, BookingStatus.Pending, BookingStatus.Completed],
+                dateFrom: dayjs().toISOString(),
+                dateTo: dayjs().subtract(1, 'minute')
+                    .toISOString()
+            })
+
+        expect(res.statusCode).to.equal(400)
+        expect(res.type).to.eq('application/json')
+    })
+})
+
+describe('POST ' + endpoint(), () => {
+    let user: User
+    let restaurant: Restaurant
+
+    before(async () => {
+        [user, restaurant] = await Promise.all([
+            prisma.user.findOne(),
+            prisma.restaurant.findOne()
+        ])
+
+        await prisma.restaurant.updateOne(restaurant.id, {
+            timeFrom: '10:00',
+            timeTo: '20:00'
+        })
+    })
+
+    it('Create booking (200)', async () => {
+        const res = await supertest(app)
+            .post(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .send({
+                restaurantID: restaurant.id,
+                guestsNumber: 3,
+                bookingTime: dayjs('13:00', 'HH:mm').add(1, 'day')
+                    .toISOString()
+            })
+
+        expect(res.statusCode).to.equal(200)
+        expect(res.type).to.eq('application/json')
+
+        const validationResult = BookingController.schemas.response.postBooking.validate(res.body)
+        expect(validationResult.error).to.eq(undefined)
+    })
+
+    it('Create booking (200) - with message', async () => {
+        const res = await supertest(app)
+            .post(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .send({
+                restaurantID: restaurant.id,
+                guestsNumber: 3,
+                bookingTime: dayjs('14:00', 'HH:mm').add(1, 'day')
+                    .toISOString(),
+                message: 'Can we have three bottles of red wine served on arrival'
+            })
+
+        expect(res.statusCode).to.equal(200)
+        expect(res.type).to.eq('application/json')
+
+        const validationResult = BookingController.schemas.response.postBooking.validate(res.body)
+        expect(validationResult.error).to.eq(undefined)
+    })
+
+    it('Create booking (400) - in the past', async () => {
+        const res = await supertest(app)
+            .post(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .send({
+                restaurantID: restaurant.id,
+                guestsNumber: 3,
+                bookingTime: dayjs('14:00', 'HH:mm').subtract(1, 'day')
+                    .toISOString(),
+                message: 'Can we have three bottles of red wine served on arrival'
+            })
+
+        expect(res.statusCode).to.equal(400)
+        expect(res.type).to.eq('application/json')
+    })
+
+    it('Create bbooking (400) - out of restaurant opening hours', async () => {
+        const res = await supertest(app)
+            .post(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .send({
+                restaurantID: restaurant.id,
+                bookingTime: dayjs('21:00', 'HH:mm').add(1, 'day')
+                    .toISOString()
+            })
+
+        expect(res.statusCode).to.equal(400)
+        expect(res.type).to.eq('application/json')
+    })
+
+    it('Create Booking (404) - restaurant does not exist', async () => {
+        const res = await supertest(app)
+            .post(endpoint())
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${JwtService.generateToken({
+                id: user.id,
+                aud: JwtAudience.b2c
+            })}`)
+            .send({
+                restaurantID: faker.string.uuid(),
+                guestsNumber: 3,
+                bookingTime: dayjs('21:00', 'HH:mm').add(1, 'day')
+                    .toISOString()
+            })
+
+        expect(res.statusCode).to.equal(404)
+        expect(res.type).to.eq('application/json')
+    })
+})
