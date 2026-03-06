2525fe8df16b437be2b0a804a929196c505a4275
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-03-06 21:21:53 +0000

    seeder were updated to generate more bookings and restaurants

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -25,7 +25,7 @@ const GEO_BOX = {
     maxLng: 0.7
 };
 
-const GRAIN = 500
+const GRAIN = 300
 
 async function seed() {
     const userData: any[] = []
@@ -39,60 +39,75 @@ async function seed() {
     // Generate plain objects
     // NOTE: Same order is used in creation
     for (let i = 0; i < GRAIN; i++) {
+        const random = Math.floor(Math.random() * 10) + 1;
+
         brandData.push(BrandGenerator.generateData({
             id: faker.string.uuid()
         }))
+
         userData.push(UserGenerator.generateData({
             id: faker.string.uuid(),
             password: crypto.SHA256('test123').toString(),
             email: `user${i.toString()}@gmail.com` 
         }))
-        addressData.push(AddressGenerator.generateData({
-            longitude: new Decimal(faker.number.float({
-                min: GEO_BOX.minLng,
-                max: GEO_BOX.maxLng
-            })),
-            latitude: new Decimal(faker.number.float({
-                min: GEO_BOX.minLat,
-                max: GEO_BOX.maxLat
+
+        for (let ai = 0; ai < random; ai++) {
+            addressData.push(AddressGenerator.generateData({
+                longitude: new Decimal(faker.number.float({
+                    min: GEO_BOX.minLng,
+                    max: GEO_BOX.maxLng
+                })),
+                latitude: new Decimal(faker.number.float({
+                    min: GEO_BOX.minLat,
+                    max: GEO_BOX.maxLat
+                })),
+                country: 'United Kingdom',
+                city: 'London'
             }))
-        }))
+        }
+
         adminData.push(AdminGenerator.generateData({
             id: faker.string.uuid(),
             password: crypto.SHA256('test123').toString(),
             email: `admin${i.toString()}@gmail.com`,
             brandID: brandData[i].id
         }))
-        restaurantData.push(RestaurantGenerator.generateData({
-            id: faker.string.uuid(),
-            name: `${i.toString()} ${faker.food.dish()} ${faker.helpers.arrayElement(['House', 'Restaurant', 'Kitchen', 'Bistro', 'Grill', 'Cafe'])}`,
-            addressID: addressData[i].id,
-            brandID: brandData[i].id
-        }))
 
-        for (let bi = 0; bi < (GRAIN / 25) + 1; bi++) {
-            const bookingTime = dayjs(faker.date.between({
-                from: timeFrom.toISOString(),
-                to: timeTo.toISOString()
-            }))
-
-            const before = [BookingStatus.Completed, BookingStatus.Cancelled, BookingStatus.NoShow]
-            const after = [BookingStatus.Approved, BookingStatus.Cancelled, BookingStatus.Pending]
-
-            let status
-            if(dayjs().isBefore(dayjs())) {
-                status = before[Math.floor(Math.random() * before.length)]
-            } else {
-                status = after[Math.floor(Math.random() * after.length)]
-            }
-
-            bookingData.push(BookingGenerator.generateData({
+        for (let ri = 0; ri < random; ri++) {
+            restaurantData.push(RestaurantGenerator.generateData({
                 id: faker.string.uuid(),
-                restaurantID: restaurantData[i].id,
-                userID: userData[i].id,
-                bookingTime: bookingTime.toDate(),
-                status: status
+                // eslint-disable-next-line max-len
+                name: `${i.toString()}-${ri.toString()} ${faker.food.dish()} ${faker.helpers.arrayElement(['House', 'Restaurant', 'Kitchen', 'Bistro', 'Grill', 'Cafe'])}`,
+                addressID: addressData[i + ri].id,
+                brandID: brandData[i].id
             }))
+        }
+
+        for (let bdi = 0; bdi < random; bdi++) {
+            for (let bi = 0; bi < (GRAIN / 20) + 1; bi++) {
+                const bookingTime = dayjs(faker.date.between({
+                    from: timeFrom.toISOString(),
+                    to: timeTo.toISOString()
+                }))
+
+                const before = [BookingStatus.Completed, BookingStatus.Cancelled, BookingStatus.NoShow]
+                const after = [BookingStatus.Approved, BookingStatus.Cancelled, BookingStatus.Pending]
+
+                let status
+                if (dayjs().isBefore(dayjs())) {
+                    status = before[Math.floor(Math.random() * before.length)]
+                } else {
+                    status = after[Math.floor(Math.random() * after.length)]
+                }
+
+                bookingData.push(BookingGenerator.generateData({
+                    id: faker.string.uuid(),
+                    restaurantID: restaurantData[i + bdi].id,
+                    userID: userData[i].id,
+                    bookingTime: bookingTime.toDate(),
+                    status: status
+                }))
+            }
         }
     }
 
