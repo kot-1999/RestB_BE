5d88a3381ad0515e2e5a7caaa6f4db4b02c36cf6
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-04-17 17:40:08 +0100

    frontendUrl was updated

--- config/test.ts
+++ config/test.ts
@@ -13,7 +13,7 @@ const options: IConfig = {
         name: 'RestBoo',
         port: process.env.PORT as string,
         env: process.env.NODE_ENV as NodeEnv,
-        frontendUrl: process.env.FRONTEND_URL ?? 'http://localhost:3055'
+        frontendUrl: process.env.FRONTEND_URL ?? 'http://localhost:3000'
     },
     s3: {
         region: process.env.S3_REGION as string,
